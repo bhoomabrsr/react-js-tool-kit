3050075bcf867abb3b161171c96c6bd03ddb3bba
bhoomabrsr <bhoomabrsr@gmail.com>
2022-04-10 09:41:20 -0500

    Update fetcher.js

--- JS/fetcher.js
+++ JS/fetcher.js
@@ -1,1 +1,28 @@
-//Quick snippet of Fetcher
+//Quick snippet of Fetch
+/*
+
+console test code:
+let getAURLData = async () => {
+    console.log('sending...');
+    const a = await fetcher('https://catfact.ninja/fact1');
+    console.log(a);
+}
+getAURLData();
+
+*/
+
+function fetcher(url, data = undefined, method = undefined) {
+  return fetch(url, {
+    method: method ? method : data ? "POST" : "GET",
+    credentials: "include",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(data),
+  })
+    .then((res) => {
+      if (res.status > 399 && res.status < 200) { throw new Error(); }
+      return res.json();
+    })
+    .catch((error) => {
+      return error.message;
+    });
+}
